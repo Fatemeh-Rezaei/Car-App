3c94819312e4983d0172f2b90c9f8672f3b0c538
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-07-01 18:29:14 +0430

    added card module

--- components/templates/CarsPage.js
+++ components/templates/CarsPage.js
@@ -1,9 +1,10 @@
+import Card from "../module/Card";
 import styles from "./CarsPage.module.css";
 
 function CarsPage({data}) {
   return <div className={styles.container}>
     {data.map((car) => (
-        <p key={car.id}>{car.name}</p>
+        <Card key={car.id} {...car} />
       ))}
   </div>;
 }
