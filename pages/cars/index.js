70e35df0887e7c8117f56ce3127d016bf10da4d9
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-07-01 19:47:01 +0430

    added categories module

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,9 +1,11 @@
+import Categories from "../../components/module/Categories";
 import CarsPage from "../../components/templates/CarsPage";
 import carsData from "../../data/carsData";
 
 function Details() {
   return (
     <div>
+      <Categories />
       <CarsPage data={carsData} />
     </div>
   );
