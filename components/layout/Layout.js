26d21586d58f995bbb85b4c5c4b83f36c8448c20
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-07-01 16:53:52 +0430

    initial project and create layout

---
+++ components/layout/Layout.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+import styles from "./Layout.module.css";
+
+function Layout({ children }) {
+  return (
+    <>
+      <header className={styles.header}>
+        <Link href="/">
+          <h2>Car App</h2>
+          <p>Choose an Buy your car</p>
+        </Link>
+      </header>
+      <div className={styles.container}>{children}</div>
+      <footer className={styles.footer}>Car App | &copy;</footer>
+    </>
+  );
+}
+
+export default Layout;
