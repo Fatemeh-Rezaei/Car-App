fe77eca86f73fa3a2d99780ea0324bb16ba5a48b
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-07-01 19:09:53 +0430

    added carDetails page

--- pages/cars/[carId].js
+++ pages/cars/[carId].js
@@ -1,13 +1,14 @@
 import { useRouter } from "next/router";
 
 import carsData from "../../data/carsData";
+import CarDetails from "../../components/templates/CarDetails";
 
 function CarDetail() {
   const router = useRouter();
   const { carId } = router.query;
   const carDetail = carsData[carId - 1];
-  
-  return <div>CarDetail</div>;
+
+  return <CarDetails {...carDetail} />;
 }
 
 export default CarDetail;
